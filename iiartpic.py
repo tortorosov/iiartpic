c84569e36768ddb4e283dbd0b4fd60d45b102120
Tortorosov <tortorosov@gmail.com>
2020-11-04 22:28:05 +0200

    Update

--- iiartpic.py
+++ iiartpic.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.graph_objects as go
 
 
-st.title("Example Project")
+#st.title("Example Project")
 
 z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
 #z_data = pd.read_csv('data.csv')
@@ -12,7 +12,7 @@ z = z_data.values
 sh_0, sh_1 = z.shape
 x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
 fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
-fig.update_layout(title='test', autosize=False,
+fig.update_layout(title='Example Project', autosize=False,
                   width=800, height=800,
                   margin=dict(l=40, r=40, b=40, t=40))
 st.plotly_chart(fig)
